31a5f33d4466c94d2327253328f1896f03f42900
Yev <str.lucky86@gmail.com>
2022-12-30 00:00:38 -0500

    added Accordion to single tournament page.

--- src/pages/tournaments/tournament.tsx
+++ src/pages/tournaments/tournament.tsx
@@ -1,10 +1,12 @@
-import { Grid } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Grid, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import GamesList from "../../components/GamesList";
 import PageTitle from "../../components/PageTitle";
 import TournamentBracket from "../../components/TournamentBracket";
 import Layout from "../../layout/layout";
 import { GetTournament } from "../../services/TournamentsService";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export interface TournamentProps {
     id: number;
@@ -32,9 +34,45 @@ const Tournament = () => {
                     <PageTitle text={tournament?.name ?? ""} />
                 </Grid>
                 <Grid item xs={12}>
-                    <Grid container spacing={2}>
-                        <TournamentBracket id={1}></TournamentBracket>
-                    </Grid>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                        <Typography>Rules</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                        <Typography>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        </Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>Games</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <GamesList />
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion defaultExpanded>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
+                        >
+                            <Typography>Bracket</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <TournamentBracket id={1}></TournamentBracket>
+                        </AccordionDetails>
+                    </Accordion>
                 </Grid>
             </Grid>
         </Layout>
