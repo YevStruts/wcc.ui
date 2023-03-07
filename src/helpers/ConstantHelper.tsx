b64d6c443671c41502eca566350202262fc9e114
Yev <str.lucky86@gmail.com>
2023-03-06 23:08:35 -0500

    added isDevelopment condition.

--- src/helpers/ConstantHelper.tsx
+++ src/helpers/ConstantHelper.tsx
@@ -1,5 +1,17 @@
-// const API = "https://wcc-cossacks.com:5002/api";
-const API = "http://wcc-cossacks.com:5001/api";
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+const API = isDevelopment ?
+    "http://localhost:5001/api" :
+    "http://wcc-cossacks.com:5001/api";
+
+// const API = isDevelopment ?
+//     "https://localhost:5002/api" :
+//     "https://wcc-cossacks.com:5002/api";
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // dev code
+} else {
+    // production code
+}
 
 export const ApiUrls = {
     player_rating: `${API}/player/list`,
